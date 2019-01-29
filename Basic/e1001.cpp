081576f8e69c8f932349c3474471c39872f3df0b
Otávio Bertucini <otaviobertucini@gmail.com>
2019-01-29 17:24:07 -0200

    Exercise added

---
+++ Basic/e1001.cpp
@@ -0,0 +1,17 @@
+//https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin >> a;
+    cin >> b;
+
+    int x = a + b;
+    cout << "X = " << x << endl;
+
+    return 0;
+}
