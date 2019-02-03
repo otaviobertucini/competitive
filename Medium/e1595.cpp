1b4c1ff9a8414868551e3aafe0bd7dd3155aed15
Otávio Bertucini <otaviobertucini@gmail.com>
2019-02-03 16:22:24 -0200

    e1595 completed

--- Medium/e1595.cpp
+++ Medium/e1595.cpp
@@ -10,19 +10,16 @@
 
 using namespace std;
 
+void make_sum(double* total_time, vector<int>* vec){
+    for(int j = 0; j < vec->size(); j++){
+        *total_time += 1.0/vec->at(j);
+    }
+}
+
 void find_lowers(int c, vector<int>* steps, int r){
-    set<int> lower;
+    sort(steps->begin(), steps->end());
     for(int i = 0; i < c; i++){
-        int smaller = 101;
-        int index_smaller;
-        for(int j = 0; j < steps->size(); j++){
-            if(steps->at(j) < smaller && !lower.count(j)){
-                smaller = steps->at(j);
-                index_smaller = j;
-            }
-        }
-        steps->at(index_smaller) += r;
-        lower.insert(index_smaller);
+        steps->at(i) += r;
     }
 }
 
@@ -37,17 +34,15 @@ int main() {
         steps.reserve(s);
 
         for(int j = 0; j < s; j++){
-            // int step;
-            // cin >> step;
-            int step = rand() % 100 + 1;
+            int step;
+            cin >> step;
+            //int step = rand() % 100 + 1;
             steps.push_back(step);
         }
 
         find_lowers(c, &steps, r);
         double total_time = 0.0;
-        for(int j = 0; j < steps.size(); j++){
-            total_time += 1.0/steps.at(j);
-        }
+        make_sum(&total_time, &steps);
         cout << std::fixed << std::setprecision(2) << total_time << endl;
     }
 
