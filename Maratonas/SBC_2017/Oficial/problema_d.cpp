23bd411b4bd227c2ce9e4abb48b86264d6295df6
otaviobertucini <otaviobertucini@gmail.com>
2019-02-27 16:28:41 -0300

    Problem D SBC-2017 completed

--- Maratonas/SBC_2017/Oficial/problema_d.cpp
+++ Maratonas/SBC_2017/Oficial/problema_d.cpp
@@ -1,67 +1,43 @@
 //http://maratona.ime.usp.br/hist/2017/prim-fase17/maratona.pdf
 
 #include <iostream>
-#include <vector>
+#include <math.h>
 
 using namespace std;
 
-/*
-Pseudo:
-Given a number n, divide n starting by 2. If it's
-divisable, go on and try to divide by the nexts
-primes. If it's not divisable by 2, try the next primes.
-If there's no prime to divide, the algo is over.
-*/
-
-// int isPrime(int n){
-//     int count = 0;
-//     if(n <= 2){
-//         return 1;
-//     }
-//     for(int i = 2; i < n; i++){
-//         if(n % i == 0){
-//             return 0;
-//         }
-//     }
-//     return 1;
-// }
-
-int isPrime(int n)
+int isDespojado(long long n)
 {
-    if (n <= 1)  return 0;
-    if (n <= 3)  return 1;
-
-    if (n%2 == 0 || n%3 == 0) return 0;
-
-    for (int i=5; i*i<=n; i=i+6)
-        if (n%i == 0 || n%(i+2) == 0)
-           return 0;
-
-    return 1;
+    if(n == 1){
+        return 0;
+    }
+    int prime = 1;
+    for(long long k = 2; k <= sqrt(n); k++){
+        if(n % (k*k) == 0){
+            return 0;
+        }
+        if(n % k == 0){
+            prime = 0;
+        }
+    }
+    return !prime;
 }
-
-//TODO: verificar resposta
 
 int main() {
 
-    int n = 252, count = 0, k = 2;
-    //cin >> n;
+    long long n, k = 1;
+    int count = 0;
+    cin >> n;
 
-    while(n != 1){
-        if(isPrime(k) && n % k == 0){
-            count++;
-            n /= k;
-        }
-        k++;
-
-        if(k == n){
-            cout << 0 << endl;
-            return 0;
+    for(; k < sqrt(n); k++){
+        if(n % k == 0){
+            count += isDespojado(k);
+            if(k != n/k){
+                count += isDespojado(n/k);
+            }
         }
     }
 
-    cout << k << endl;
-
+    cout << count << endl;
 
     return 0;
 }
