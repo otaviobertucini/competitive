e4ea3c26c3d872128170a2e1ebf233fb7395ab92
otaviobertucini <otaviobertucini@gmail.com>
2019-02-27 17:00:05 -0300

    Problem G SBC-2017 started

---
+++ Maratonas/SBC_2017/Oficial/problema_g.cpp
@@ -0,0 +1,24 @@
+//http://maratona.ime.usp.br/hist/2017/prim-fase17/maratona.pdf
+
+#include <iostream>
+
+/*TODO: criar uma função recursiva para analisar os resultados.
+OBS: não é a resposta do programa! é só para visualizar.
+*/
+
+using namespace std;
+void generate(int t, int m, int n, int tam = 0){
+
+}
+
+int main() {
+
+    int t, m, n;
+
+    cin >> t >> m >> n;
+    int count = 0;
+
+
+
+    return 0;
+}
